a996b9ab8e757c15d8aec81c86cd41ab576ef3a6
ilgiz <4ilgiz@gmail.com>
2019-01-24 15:16:50 +0300

    Second commit

--- src/cf/tilgiz/Main.java
+++ src/cf/tilgiz/Main.java
@@ -3,6 +3,11 @@ package cf.tilgiz;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int x = Math.summ(2,6);
+        System.out.println(x);
+        Math num = new Math();
+        System.out.println(num.mult(3,5));
+        System.out.println(Math.z);
+
     }
 }
---
+++ src/cf/tilgiz/Math.java
@@ -0,0 +1,15 @@
+package cf.tilgiz;
+
+public final class Math {
+
+    static int z;
+
+    static int summ (int a, int b){
+        return a + b;
+    }
+
+    int mult (int a, int b){
+        return a * b;
+    }
+}
+
